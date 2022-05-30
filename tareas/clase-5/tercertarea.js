665f512e01a7d21502742c6450be45faa4c0e17d
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-05-30 18:13:54 -0300

    Actualiza el código en base a las clases cambiadas

--- tareas/clase-5/tercertarea.js
+++ tareas/clase-5/tercertarea.js
@@ -1,29 +1,29 @@
 const $calcularTiempoTotal = document.querySelector('#calcular-tiempo-total');
 
 $calcularTiempoTotal.onclick = function() {
-    const $horas = document.querySelectorAll('#horas');
-    const $minutos = document.querySelectorAll('#minutos');
-    const $segundos= document.querySelectorAll('#segundos');
+    const $horas = document.querySelectorAll('.horas');
+    const $minutos = document.querySelectorAll('.minutos');
+    const $segundos= document.querySelectorAll('.segundos');
     let horasTotal = 0;
     let minutosTotal = 0;
     let segundosTotal = 0;
 
-    for(let i=0; i < segundos.length; i++) {
-        segundosTotal += Number(segundos[i].value);
+    for(let i=0; i < $segundos.length; i++) {
+        segundosTotal += Number($segundos[i].value);
     } if (segundosTotal > 60) {
         minutosTotal = Math.trunc(segundosTotal/60);
         segundosTotal = Math.trunc(segundosTotal%60);
     }
 
-    for(let i=0; i < minutos.length; i++) {
-        minutosTotal += Number(minutos[i].value);
+    for(let i=0; i < $minutos.length; i++) {
+        minutosTotal += Number($minutos[i].value);
     } if(minutosTotal > 60){
         horasTotal = Math.trunc(minutosTotal/60);
         minutosTotal = Math.trunc(minutosTotal%60);
     }
 
-    for(let i=0; i < horas.length; i++) {
-        horasTotal += Number(horas[i].value);
+    for(let i=0; i < $horas.length; i++) {
+        horasTotal += Number($horas[i].value);
     }
 
     let $tiempoTotal = document.querySelector('#resultado');
