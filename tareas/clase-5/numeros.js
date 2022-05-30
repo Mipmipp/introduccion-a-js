f35039fabedc3a8d4c206f9cafd0d68b9641d491
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-05-30 06:49:26 -0300

    Crea función para calcular promedio del array

--- tareas/clase-5/numeros.js
+++ tareas/clase-5/numeros.js
@@ -3,3 +3,11 @@ const listaNumeros = [];
 for(let i = 0; i < (document.querySelectorAll('li').length); i++) {
     listaNumeros.push(Number(document.querySelectorAll('li')[i].innerText));
 }
+function calcularPromedio() {
+    let sumaNumeros = 0;
+    
+    for(let i = 0; i < (listaNumeros.length); i++) {
+        sumaNumeros += listaNumeros[i];
+    }
+    return sumaNumeros / listaNumeros.length;
+}
