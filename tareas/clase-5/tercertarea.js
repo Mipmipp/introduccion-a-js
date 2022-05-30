8eb8762f257c0c0afdec0e33c1135731c274abfd
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-05-30 05:16:19 -0300

    Crea constantes y variables

--- tareas/clase-5/tercertarea.js
+++ tareas/clase-5/tercertarea.js
@@ -1,6 +1,12 @@
 const $calcularTiempoTotal = document.querySelector('#calcular-tiempo-total');
 
 $calcularTiempoTotal.onclick = function() {
+    const $horas = document.querySelectorAll('#horas');
+    const $minutos = document.querySelectorAll('#minutos');
+    const $segundos= document.querySelectorAll('#segundos');
+    let horasTotal = 0;
+    let minutosTotal = 0;
+    let segundosTotal = 0;
 
     return false;
 }
