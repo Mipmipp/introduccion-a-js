c513772328f29c78d1d35d1aac15d57b24714b72
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-05-30 02:08:52 -0300

    Da utilidad al botón y la caja

---
+++ tareas/clase-5/archivo.js
@@ -0,0 +1,10 @@
+document.querySelector('#ingresar-información').onclick = function() {
+    const primerNombre = document.querySelector('#primer-nombre').value;
+    const segundoNombre = document.querySelector('#segundo-nombre').value;
+    const apellido = document.querySelector('#apellido').value;
+    const edad = Number(document.querySelector('#edad').value);
+
+    document.querySelector('#información-personal').value = `${primerNombre}, ${segundoNombre}, ${apellido}, ${edad} años.`;
+
+    return false;
+}
