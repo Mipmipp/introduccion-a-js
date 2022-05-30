849de8d5e71bdffd4409cd508c1b2a373c70df29
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-05-30 05:17:28 -0300

    Crea bucles que sumen la información del formulario

--- tareas/clase-5/tercertarea.js
+++ tareas/clase-5/tercertarea.js
@@ -8,5 +8,22 @@ $calcularTiempoTotal.onclick = function() {
     let minutosTotal = 0;
     let segundosTotal = 0;
 
+    for(let i=0; i < segundos.length; i++) {
+        segundosTotal += Number(segundos[i].value);
+    } if (segundosTotal > 60) {
+        minutosTotal = Math.trunc(segundosTotal/60);
+        segundosTotal = Math.trunc(segundosTotal%60);
+    }
+
+    for(let i=0; i < minutos.length; i++) {
+        minutosTotal += Number(minutos[i].value);
+    } if(minutosTotal > 60){
+        horasTotal = Math.trunc(minutosTotal/60);
+        minutosTotal = Math.trunc(minutosTotal%60);
+    }
+
+    for(let i=0; i < horas.length; i++) {
+        horasTotal += Number(horas[i].value);
+    }
     return false;
 }
