a0abd2750b495e27941b51900fb32e0a603d0555
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-05-30 18:19:46 -0300

    Crea función para convertir el array de en base a 'li'
    El bucle estaba en el aire y se decidió pasarlo a una función

--- tareas/clase-5/numeros.js
+++ tareas/clase-5/numeros.js
@@ -1,7 +1,9 @@
 const listaNumeros = [];
 
-for(let i = 0; i < (document.querySelectorAll('li').length); i++) {
-    listaNumeros.push(Number(document.querySelectorAll('li')[i].innerText));
+function convertirArray() {
+    for(let i = 0; i < (document.querySelectorAll('li').length); i++) {
+        listaNumeros.push(Number(document.querySelectorAll('li')[i].innerText));
+    }
 }
 
 function calcularPromedio() {
