0bd7de0a406d8d8b0ab8bc66cb4b91e09c434882
Mipmipp <106466126+Mipmipp@users.noreply.github.com>
2022-08-19 03:35:20 -0300

    Crea función para mostrar/ocultar elementos
    Se usa para mostrar u ocultar los botones o interfaces pre-creadas en base a las acciones del usuario en la página

---
+++ tareas/clase-6/tarea-interfaz.js
@@ -0,0 +1,7 @@
+function mostrarElemento(elemento) {
+    document.querySelector(elemento).className = '';
+}
+
+function ocultarElemento(elemento) {
+    document.querySelector(elemento).className = 'oculto';
+}
